cd2052029f72f3f9fba1a9e055f3dc8bfde2f8f5
Sora(HinataSato) <hinata.act.2@gmail.com>
2023-03-25 05:38:46 +0900

    :tada: 重力処理

---
+++ Assets/Scripts/Constants/GranvityDirectionType.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Constants
+{
+    public enum GravityDecisionType
+    {
+        /// <summary>
+        /// 上方向
+        /// </summary>
+        GravityUp,
+        /// <summary>
+        /// 下方向
+        /// </summary>
+        GravityDown,
+        /// <summary>
+        /// 左方向
+        /// </summary>
+        GravityLeft,
+        /// <summary>
+        /// 右方向
+        /// </summary>
+        GravityRight,
+    }
+}
